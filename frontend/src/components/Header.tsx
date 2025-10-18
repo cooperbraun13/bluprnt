3b5aa364325be31bf185b451dca3986e94f4034e
ADH-17 <ayden.humphries@gmail.com>
2025-10-18 15:46:00 -0700

    feat[home button] can get to and from the home page via the BluPrnt logo.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+import './Header.css';
+
 export default function Header() {
   return (
     <div className="heading">
-      <h1>BluPrnt</h1>
+      <h1><Link to="http://localhost:5173/">BluPrnt</Link></h1>
       <p className="tagline">Your blueprint for success</p>
     </div>
   );
