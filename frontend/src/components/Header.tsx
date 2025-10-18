abc3039d086c367a41d0ab95b49ce5dbe3bf7ef3
cooperbraun13 <cooperjbraun13@gmail.com>
2025-10-18 16:01:32 -0700

    feat: merged everything up

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
 
 export default function Header() {
   return (
     <div className="heading">
-      <h1><Link to="http://localhost:5173/">BluPrnt</Link></h1>
+      <h1>
+        <Link to="http://localhost:5173/">BluPrnt</Link>
+      </h1>
       <p className="tagline">Your blueprint for success</p>
     </div>
   );
