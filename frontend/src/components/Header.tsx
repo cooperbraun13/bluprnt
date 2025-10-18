67d6a431887df44b4087c458fbe59672ef077059
ADH-17 <ayden.humphries@gmail.com>
2025-10-18 15:29:08 -0700

    fix[header] made the header a tagline and removed the subpage tagline

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="heading">
       <h1>BluPrnt</h1>
-      <p>Your blueprint for success</p>
+      <p className="tagline">Your blueprint for success</p>
     </div>
   );
 }
