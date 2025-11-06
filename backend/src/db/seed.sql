4d5025e213e5df10fd27d878f6f68e19cdff35ba
cooperbraun13 <cooperjbraun13@gmail.com>
2025-11-05 17:01:36 -0800

    feat: added coopers db schema to main

--- backend/src/db/seed.sql
+++ backend/src/db/seed.sql
@@ -1,1 +1,14 @@
 /* seed data (test data) */
+
+-- Dummy data values for testing
+INSERT INTO users VALUES('John', 'Carpenter', 1, 'example@email.com', 'password', NULL);
+
+INSERT INTO vendors VALUES(1, 'Dunn Lumber', 'Lumber');
+
+INSERT INTO products VALUES(1, 'Hardwood', 'Flooring', 1, 10, 12.50);
+
+INSERT INTO projects VALUES(1, 1, 'Bathroom Floor');
+
+INSERT INTO project_items VALUES(1, 1, 100);
+
+INSERT INTO cart_items VALUES(1, 1, 100);
