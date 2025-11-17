2a97a9ff90531b0f967ebe23b5857abb887c113e
SAMV <samwv@icloud.com>
2025-11-17 14:59:02 -0800

    First Vendor Insert Statements

--- backend/src/db/seed.sql
+++ backend/src/db/seed.sql
@@ -4,6 +4,9 @@
 INSERT INTO users VALUES('John', 'Carpenter', 1, 'example@email.com', 'password', NULL);
 
 INSERT INTO vendors VALUES(1, 'Dunn Lumber', 'Lumber');
+INSERT INTO vendors VALUES(2, 'Home Depot', 'General Retail');
+INSERT INTO vendors VALUES(3, 'Lowes', 'General Retail');
+INSERT INTO vendors VALUES(4, 'Ace Hardware', 'General Retail');
 
 INSERT INTO products VALUES(1, 'Hardwood', 'Flooring', 1, 10, 12.50);
 
