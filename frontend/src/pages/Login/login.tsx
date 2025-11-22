80ffafaaa79ad22e3d8611111bdfe2f7e7523431
ADH-17 <ayden.humphries@gmail.com>
2025-11-22 11:24:22 -0800

    feat[form] basic login form is created

--- frontend/src/pages/Login/login.tsx
+++ frontend/src/pages/Login/login.tsx
@@ -1,13 +1,39 @@
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header';
 
-export default function Login(){
-    return <>
-        <Header></Header>
-        <form>
-                
-        </form>
-        <Footer></Footer>
+export default function Login() {
+  return (
+    <>
+      <Header title="Login" showTagline={false} />
+
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            placeholder="Enter your email"
+            required 
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            placeholder="Enter your password"
+            required 
+          />
+        </div>
+
+        <div className="sign_up"></div>
+
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
     </>
+  );
 }
-
